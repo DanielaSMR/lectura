f96820c1a3de73e33cbf3b7a432214056c54f325
DanielaSMR <10828775@ieselgrao.org>
2024-03-26 13:08:59 +0100

    g

--- src/ejer6.java
+++ src/ejer6.java
@@ -9,44 +9,43 @@ import java.util.Scanner;
 public class ejer6{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
-        System.out.println("Introduce un coche\nEscribe la matricula");
+        /*System.out.println("Introduce un coche\nEscribe la matricula");
         String matricula = sc.nextLine();
         System.out.println("Escribe la marca");
         String marca = sc.nextLine();
         System.out.println("Escribe el modelo");
         String modelo = sc.nextLine();
         System.out.println("Escribe el tamaño del desposito");
-        Double tamaño = sc.nextDouble();
+        Double tamaño = sc.nextDouble();*/
+        System.out.println("Introduce un coche\nEscribe la matricula");
+        String matricula = "MATRIfasdf";
+        System.out.println("Escribe la marca");
+        String marca = "MARCA";
+        System.out.println("Escribe el modelo");
+        String modelo = "MODELO";
+        System.out.println("Escribe el tamaño del desposito");
+        Double tamaño = 35.0;
 
         try{
-            FileInputStream fis = new FileInputStream("src/ejer3-1/ejer6.txt");
-            DataInputStream dis = new DataInputStream(fis);
-            FileOutputStream fos = new FileOutputStream("src/ejer3-1/ejer6.txt", true);
+           
+            FileOutputStream fos = new FileOutputStream("src/ejer3-1/ejer6.bin", true);
             DataOutputStream dos = new DataOutputStream(fos);
-
-            /*FileWriter fw = new FileWriter("src/ejer3-1/ejer6.txt", true);
-            BufferedWriter bw = new BufferedWriter(fw);
-
-            
-            bw.write("\nEl vehículo tiene una matrícula " + matricula);
-            bw.write(" Su marca es " + marca);
-            bw.write(" Su modelo es " + modelo);
-            bw.write(" El tamaño del deposito es "+ tamaño);*/
 
             dos.writeUTF("\nEl vehículo tiene una matrícula " + matricula);
             dos.writeUTF("Su marca es " + marca);
             dos.writeUTF("Su modelo es " + modelo);
             dos.writeUTF("El tamaño del deposito es "+ tamaño);
-
-            
-            //bw.close();
-            //fw.close();
-            int i;
-            while((i=dis.read()) !=-1){
-               System.out.print(dis.readUTF());
-            }
             dos.close();
             fos.close();
+
+            FileInputStream fis = new FileInputStream("src/ejer3-1/ejer6.bin");
+            DataInputStream dis = new DataInputStream(fis);
+            int i;
+            System.out.println(dis.readUTF());
+            /*while((i=dis.read()) !=-1){
+               System.out.print(dis.readUTF());
+            }*/
+            
             dis.close();
             fis.close();
 
